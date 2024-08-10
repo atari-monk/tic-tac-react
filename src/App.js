0a1b46359c52ef70c90e5258c84f750354c478c3
atari-monk <atari.monk1@gmail.com>
2024-08-10 21:54:21 +0200

    game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import "./components/Game.css";
+import Game from "./components/Game";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <header className="App-header">
+                <Game></Game>
+            </header>
         </div>
     );
 }
